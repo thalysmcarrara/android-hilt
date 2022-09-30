4bfc3b7aa3cb879d84042582addacf5f43bf0b63
thalys matias carrara <thalys.carrara@profusion.mobi>
2022-09-30 17:12:59 -0400

    refactor: implement ui tests with hilt
    - implement ui tests with hilt

--- app/src/androidTest/java/com/example/android/hilt/AppTest.kt
+++ app/src/androidTest/java/com/example/android/hilt/AppTest.kt
@@ -25,12 +25,20 @@ import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.android.hilt.ui.MainActivity
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
 import org.hamcrest.Matchers.containsString
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
+@HiltAndroidTest
 class AppTest {
+
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
     @Test
     fun happyPath() {
         ActivityScenario.launch(MainActivity::class.java)
